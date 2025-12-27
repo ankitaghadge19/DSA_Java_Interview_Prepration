c873490ae00c864203e64bec6074c3daf7562e1a
Ankita <ghadge.ankita197@gmail.com>
2025-12-27 19:31:21 +0530

    cumulative sum approach

---
+++ Contiguous_Array/BruteForceSolution.java
@@ -0,0 +1,79 @@
+package Contiguous_Array;
+
+public class BruteForceSolution {
+    public static void main(String[] args) {
+        int[] nums = {0,1,1,1,1,1,0,0,0};
+
+        int i=0; 
+        int j=0;
+        for(int p=0; p<nums.length; p++){
+            int m=0; 
+            int n=0;
+            for(int q=p; q<nums.length; q++){
+                if(nums[q] == 0){
+                    m++;
+                }else if(nums[q] == 1){
+                    n++;
+                }
+                if((m==n) && (m>i) && (n>j)){
+                    i=m;
+                    j=n;
+                }
+            }
+        }
+        System.out.println(i+j);
+
+        // Solution:
+        // Idx-  0 1 2 3 4 5 6
+        // Eg.   1 0 1 1 0 1 1 
+
+        // startIdx = 0             0    1
+        //     1                   m=0, n=1
+        //     1 0                 m=1, n=1
+        //     1 0 1               m=1, n=2
+        //     1 0 1 1             m=1, n=3
+        //     1 0 1 1 0           m=2, n=3
+        //     1 0 1 1 0 1         m=2, n=4
+        //     1 0 1 1 0 1 1       m=2, n=5
+        // Max Len = (m=1, n=1) -> (1+1) = 2
+
+        // startIdx = 1
+        //     0                   m=1, n=0
+        //     0 1                 m=1, n=1
+        //     0 1 1               m=1, n=2
+        //     0 1 1 0             m=2, n=2
+        //     0 1 1 0 1           m=2, n=3
+        //     0 1 1 0 1 1         m=2, n=4
+        // Max Len = (m=2, n=2) -> (2+2) = 4
+
+        // startIdx = 2
+        //     1                   m=0, n=1
+        //     1 1                 m=0, n=2
+        //     1 1 0               m=1, n=2
+        //     1 1 0 1             m=1, n=3
+        //     1 1 0 1 1           m=1, n=4
+        // Max Len = (Not Found)
+
+        // startIdx = 3
+        //     1                   m=0, n=1
+        //     1 0                 m=1, n=1
+        //     1 0 1               m=1, n=2
+        //     1 0 1 1             m=1, n=3
+        // Max Len = (m=1, n=1) -> (1+1) = 2
+
+        // startIdx = 4
+        //     0                   m=1, n=0
+        //     0 1                 m=1, n=1
+        //     0 1 1               m=1, n=2
+        // Max Len = (m=1, n=1) -> (1+1) = 2
+ 
+        // startIdx = 5
+        //     1                   m=0, n=1
+        //     1 1                 m=0, n=2
+        // Max Len = (Not Found)
+
+        // startIdx = 6
+        //     1                   m=0, n=1
+        // Max Len = (m=1, n=1) -> (1+1) = 2
+    } 
+}
---
+++ Contiguous_Array/OptimalSolution.java
@@ -0,0 +1,51 @@
+package Contiguous_Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OptimalSolution {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,0,0,0,1,1};
+        int maxLen = 0;
+        //Replace 0 by -1
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] == 0){
+                nums[j] = -1;
+            }
+        }
+        Map<Integer, Integer> sumIdx = new HashMap<>();
+        sumIdx.put(0, -1);  // If sum=0 (eg. [0,1])
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            if (sumIdx.keySet().contains(sum)) {
+                int idx = sumIdx.get(sum);
+                int len = i - idx;
+                if (len > maxLen) {
+                    maxLen = len;
+                }    
+            }else {
+                sumIdx.put(sum, i);
+            }
+        }
+        System.out.println(maxLen);
+ 
+        // Solution:
+        // Idx: 0   1   2   3   4   5   6   7
+        // Eg.  0   0   1   0   0   0   1   1
+        //     -1  -1   1  -1  -1  -1   1   1 (Replace 0 by -1)
+        //                                            HashMap
+        //                                          Sum      Idx
+        //                                          -1        0
+        //                                           1       -2
+        // Idx     Sum
+        //  0      -1
+        //  1      -2
+        //  2      -1
+        // Now, when we are at index 2, we get the sum as -1 again. 
+        // This means that between index 0 and index 2, the same number of 0's were subtracted and 1's were added, 
+        // resulting in no net change in the sum.
+        // length = 2 - 0 = 2
+    }
+}
