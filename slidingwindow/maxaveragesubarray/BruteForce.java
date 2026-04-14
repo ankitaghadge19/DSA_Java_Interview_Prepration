383a226199e295e1efcce6f4dfcad846a4cafa1c
Ankita <ghadge.ankita197@gmail.com>
2026-04-14 20:53:43 +0530

    feat: add brute force solution for max average subarray

---
+++ slidingwindow/maxaveragesubarray/BruteForce.java
@@ -0,0 +1,26 @@
+package slidingwindow.maxaveragesubarray;
+
+public class BruteForce {
+        public static void main(String[] args) {
+                     //    0   1   2   3   4  5
+            int[] nums = { 1, 12, -5, -6, 50, 3 };
+            int k = 4;
+
+            double result = Integer.MIN_VALUE;
+            for (int s = 0; s <= nums.length - k; s++) {
+                double sum = 0;
+                for (int e = s; e-s+1 <= k; e++) {
+                    sum = sum + nums[e];
+                }
+                double avg = sum / k;
+                if (avg > result) {
+                    result = avg;
+                }
+
+            }
+            System.out.println(result);
+        }       
+}
+
+// SC: O(1)
+// TC: O(N^2)
