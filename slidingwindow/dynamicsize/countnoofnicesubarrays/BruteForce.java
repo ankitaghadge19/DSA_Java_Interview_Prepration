ec1a0bc460eb206b1eab329e11d066b927bc0171
Ankita <ghadge.ankita197@gmail.com>
2026-04-19 21:23:03 +0530

    feature: add brute force soln for count nice sub arrays

---
+++ slidingwindow/dynamicsize/countnoofnicesubarrays/BruteForce.java
@@ -0,0 +1,25 @@
+package slidingwindow.dynamicsize.countnoofnicesubarrays;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        int[] nums = { 2, 2, 2, 1, 2, 2, 1, 2, 2, 2 };
+        int k = 2;
+        int cnt = 0;
+
+        for (int s = 0; s < nums.length; s++) {
+            int odd = 0;
+            for (int e = s; e < nums.length; e++) {
+                if (nums[e] % 2 != 0) {
+                    odd++;
+                }
+                if (odd == k) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+
+// TC: O(N^2)
+// SC: O(1)
