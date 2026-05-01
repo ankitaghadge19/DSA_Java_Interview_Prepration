c79f707bdac07adaf328fa111df710268a845c9e
Ankita <ghadge.ankita197@gmail.com>
2026-05-01 23:05:02 +0530

    feat: add brute force solution for two sum

---
+++ arrays/medium/twosum/BruteForce.java
@@ -0,0 +1,29 @@
+package arrays.medium.twosum;
+
+public class BruteForce {
+    public static void main() {
+        int[] nums = { 3,2,4 };
+        int target = 6;
+
+        int[] result = twoSum(nums, target);
+        
+        for (int n : result) {
+            System.out.println(n);
+        }
+    }
+
+    static int[] twoSum(int[] nums, int target){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
+
+// SC: O(1)
+// TC: O(N^2)
