fe0e426c44a1900ae3282dea5597b66bf7b31491
Ankita <ghadge.ankita197@gmail.com>
2026-06-06 21:40:19 +0530

    feat: add brute force solution of count subarrays eum equals k

---
+++ arrays/medium/countsuarraysumequalsk/BruteForce.java
@@ -0,0 +1,25 @@
+package arrays.medium.countsuarraysumequalsk;
+
+import java.util.HashMap;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        int[] nums = { 10, 5, 2, 7, 1, -10 };
+        int k = 15;
+
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum = sum + nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+// TC: O(1)
+// SC: O(N^2)
