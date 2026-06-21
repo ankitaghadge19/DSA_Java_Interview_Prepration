164c1bdcf75e8f1e886c9d4d287f964d7f7ca232
Ankita <ghadge.ankita197@gmail.com>
2026-06-21 08:41:58 +0530

    feat: add solution using set for merge two sorted arrays and return new array

---
+++ arrays/easy/unionoftwosortedarraysreturnnewarray/UsingSet.java
@@ -0,0 +1,23 @@
+package arrays.easy.unionoftwosortedarraysreturnnewarray;
+
+import java.util.TreeSet;
+
+public class UsingSet {
+    public static void main() {
+        int[] arr1 = { 1, 2, 3, 4, 5 };
+        int[] arr2 = { 2, 3, 4, 4, 5 };
+        TreeSet<Integer> result = new TreeSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            result.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            result.add(arr2[i]);
+        }
+        System.out.println(result);
+    }
+}
+
+// TC: O(n log n + m log m)
+// SC: O(n + m)
+// n is length of array 1
+// m is length of array 2
