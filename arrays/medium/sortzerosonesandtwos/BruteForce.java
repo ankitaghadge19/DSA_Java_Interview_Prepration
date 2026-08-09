550b4964bb50317a1e2e8ae67e06c836c873d1aa
Ankita <ghadge.ankita197@gmail.com>
2026-08-09 21:49:10 +0530

    add brute force solution for sort 0, 1 and 2

---
+++ arrays/medium/sortzerosonesandtwos/BruteForce.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        int[] nums = { 1, 0, 2, 1, 0 };
+
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeros++;
+            } else if (nums[i] == 1) {
+                ones++;
+            } else if (nums[i] == 2) {
+                twos++;
+            }
+        }
+
+        int i = 0;
+        while (zeros > 0) {
+            nums[i] = 0;
+            zeros--;
+            i++;
+        }
+
+        while (ones > 0) {
+            nums[i] = 1;
+            ones--;
+            i++;
+        }
+
+        while (twos > 0) {
+            nums[i] = 2;
+            twos--;
+            i++;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
+// Problem Link: https://leetcode.com/problems/sort-colors/description/
+
+// TC: O(N)
+// SC: O(1)
+
+// Approach:
+// Count the number of 0s, 1s, and 2s
+// Traverse the array again and overwrite it with all 0s, then 1s, then 2s
+// This sorts the array without using extra space
