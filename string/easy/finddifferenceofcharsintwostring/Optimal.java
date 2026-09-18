e551c59fde743732e9d52fc74861592a2f51cacc
Ankita <ghadge.ankita197@gmail.com>
2026-09-18 21:47:42 +0530

    use xor to find different char

---
+++ string/easy/finddifferenceofcharsintwostring/Optimal.java
@@ -0,0 +1,36 @@
+package string.easy.finddifferenceofcharsintwostring;
+
+public class Optimal {
+    public static void main(String[] args) {
+        String s = "abcde";
+        String t = "abcd";
+
+        // XOR
+        // a ^ a = 0
+        // a ^ 0 = a
+
+        // = (a ^ b ^ c ^ d) ^ (a ^ b ^ c ^ d ^ e)
+        // = (a ^ a) ^ (b ^ b) ^ (c ^ c) ^ (d ^ d) ^ e
+        // =    0    ^    0    ^    0    ^    0    ^ e
+        // = e
+
+        // 0 ^ a = a
+        char result = 0;
+
+        // XOR all chars of s
+        for (char ch : s.toCharArray()) {
+            result = (char) (result ^ ch);
+        }
+
+        // XOR all chars of t
+        for (char ch : t.toCharArray()) {
+            result = (char) (result ^ ch);
+        }
+        System.out.println(result);
+    }
+}
+
+// TC: O(N)
+// SC: O(1)
+
+// Problem Link: https://leetcode.com/problems/find-the-difference/description/
